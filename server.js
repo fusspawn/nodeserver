465ad1aa4aab6ecbbae97ec9aae22ade7a7779b3
Paul Thornton <ariochofmelnibone@gmail.com>
2011-03-01 23:02:59 +0000

    added logging

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World from Duostack! - Fusspawn Style \n');
+  console.log("served default page at: "+ new Date());
+  
 }).listen(8124);
 
 console.log('Server running at http://127.0.0.1:8124/');
