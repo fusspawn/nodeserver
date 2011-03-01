365de931cbf204c54ad65226669518bc490d92d4
Paul Thornton <ariochofmelnibone@gmail.com>
2011-03-01 22:57:42 +0000

    More Awesome

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World from Duostack!\n');
+  res.end('Hello World from Duostack! - Fusspawn Style \n');
 }).listen(8124);
 
 console.log('Server running at http://127.0.0.1:8124/');
