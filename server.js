7863a64ff5832e0ded8d8cb92272a716330db762
Paul Thornton <ariochofmelnibone@gmail.com>
2011-03-01 23:54:22 +0000

    added deps

--- server.js
+++ server.js
@@ -15,9 +15,10 @@ var db = {
 var DB = mongo.Db(db.name, new mongo.Server(db.host, db.port, {}), {});
 
 DB.open(function(err, db) {
-	var object = {name: "testing"};
-	db.items.save(object);
-	
+	console.log("db opened");
+	db.close(function(err, db) {
+		console.log("db closed");
+	});
 });
 
 
