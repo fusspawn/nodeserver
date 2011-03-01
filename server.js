873b994a1f5c1eb42adb7a2869a338ea4a86f973
Paul Thornton <ariochofmelnibone@gmail.com>
2011-03-01 23:47:22 +0000

    added deps

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 var http = require('http');
+var mongo = require("mongodb");
 var orm = require("mongoose");
+var parseURL = require("url").parse;
+var components = parseUrl(process.env['DUOSTACK_DB_MONGODB']);
+var db = {
+  host: components.hostname,
+  port: parseInt(components.port),
+  name: components.pathname.substr(1),
+  user: components.auth.split(':')[0],
+  pass: components.auth.split(':')[1]
+};
 
 
+var DB = mongo.Db(name, new mongo.Server(host, port, {}), {});
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World from Duostack! - Fusspawn Style \n');
   console.log("served default page at: "+ new Date());
-  console.log("mongoose data: "+ JSON.stringify(orm));
+  console.log("mongoose data: "+orm);
   
 }).listen(80);
 
