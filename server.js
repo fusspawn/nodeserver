45b1e0d1afc661bb1da8c7bfbc456db43f6b3399
Paul Thornton <ariochofmelnibone@gmail.com>
2011-03-02 00:38:30 +0000

    added deps

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 var http = require('http');
 var mongo = require("mongodb");
-var mongoose = require('mongoose/').Mongoose,
-	db_uri = process.env['DUOSTACK_DB_MONGODB'],
-    db = new mongoose(),
-	db_conn = db.connect(db_uri);
-    Model = db_conn.noSchema('test', db_conn); // collection name
-
+var mongoose = require('mongoose'),
+	db_uri = process.env['DUOSTACK_DB_MONGODB'];
+	mongoose.connect(db_uri);
+	
+var Schema = mongoose.Schema,
+	ObjectId = Schema.ObjectId;
+	
+var TestSchema = new Schema({path: String, date: Date});
+	mongoose.model("test", TestSchema);
+	
+	
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   console.log("served default page at: "+ new Date()); 
-  var db_item = new Model({path: "/", date: new Date()});
-  db_item.save();
+  var item = new TestSchema();
+  item.path = "/";
+  item.date = new Date();
+  item.save(function(err) {
+	res.puts(err);
+  });
   
   Model.find({}).each(function(model){
 	res.puts(model.path + " on: "+ model.date);
