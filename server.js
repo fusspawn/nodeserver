275ee8dc6605128f5975d34f30325a8edfe0bb90
Paul Thornton <ariochofmelnibone@gmail.com>
2011-03-02 00:41:43 +0000

    added deps

--- server.js
+++ server.js
@@ -14,18 +14,15 @@ var TestSchema = new Schema({path: String, date: Date});
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   console.log("served default page at: "+ new Date()); 
-  var item = new TestSchema();
+  var model = mongoose.model("test");
+  var item = new model();
   item.path = "/";
   item.date = new Date();
   item.save(function(err) {
 	res.puts(err);
-  });
+  });  
   
-  Model.find({}).each(function(model){
-	res.puts(model.path + " on: "+ model.date);
-  },hydrate);
-  
-  res.end('Hello World from Duostack! - Fusspawn Style \n');
+  res.end('Hello World from Duostack! - Fusspawn Style - This request was logged to mongodb \n');
 }).listen(80);
 
 console.log('Server running at http://127.0.0.1:8124/');
