f0bfcaccb43ead2e9934c844d31495ee416bdbc6
Paul Thornton <ariochofmelnibone@gmail.com>
2011-03-02 00:17:22 +0000

    added deps

--- server.js
+++ server.js
@@ -1,28 +1,21 @@
 var http = require('http');
 var mongo = require("mongodb");
-var orm = require("mongoose");
-var parseURL = require("url").parse;
-var components = parseURL(process.env['DUOSTACK_DB_MONGODB']);
-var db = {
-  host: components.hostname,
-  port: parseInt(components.port),
-  name: components.pathname.substr(1),
-  user: components.auth.split(':')[0],
-  pass: components.auth.split(':')[1]
-};
 
-
-var DB = mongo.Db(db.name, new mongo.Server(db.host, db.port, {}), {});
-console.log(DB);
-
-
+var mongoose = require('mongoose/').Mongoose,
+    db = mongoose.connect(process.env['DUOSTACK_DB_MONGODB']),      
+    Model = mongoose.noSchema('test', db); // collection name
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World from Duostack! - Fusspawn Style \n');
-  console.log("served default page at: "+ new Date());
-  console.log("mongoose data: "+orm);
+  console.log("served default page at: "+ new Date());, 
+  var db_item = new Model({path: "/", date: new Date()});
+  db_item.save();
   
+  Model.find({}).each(function(model){
+	res.puts(model.path + " on: "+ model.date);
+  },hydrate);
+  
+  res.end('Hello World from Duostack! - Fusspawn Style \n');
 }).listen(80);
 
 console.log('Server running at http://127.0.0.1:8124/');
