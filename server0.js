be2f615c3a5965a1e4c3331331a5a1bd7a925c5b
Paul Thornton <ariochofmelnibone@gmail.com>
2011-03-02 02:04:56 +0000

    added deps

---
+++ server0.js
@@ -0,0 +1,52 @@
+var sys = require("sys");
+var http = require('http');
+var express = require("express");
+var mongo = require("mongodb");
+
+var mongoose = require('mongoose'),
+	db_uri = process.env['DUOSTACK_DB_MONGODB'];
+	mongoose.connect(db_uri);
+	
+var Schema = mongoose.Schema,
+	ObjectId = Schema.ObjectId;
+	
+var TestSchema = new Schema({path: String, date: Date});
+	mongoose.model("test", TestSchema);
+	
+var app = express.createServer();
+app.get('/', function(req, res){
+  console.log("served default page at: "+ new Date()); 
+  var model = mongoose.model("test");
+  var item = new model();
+  item.path = "/";
+  item.date = new Date();
+  item.save(function(err) {
+	console.log(err);
+  });  
+  
+  res.write("<html><body><ul>");
+  model.find({}, function(err, docs) {
+	 docs.forEach(function(doc) {
+              res.write("<li>" + doc.path + " on" + doc.date + "</li>");
+	 });
+	 res.end('</ul></body></html>');
+  });
+});
+
+app.get("/test/", function(req, res) {
+	res.write("test");
+	res.end("end");
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	var model = mongoose.model("test");
+	var item = new model();
+	item.path = "/";
+	item.date = new Date();
+	
+	item.save(function(err) {
+		console.log(err);
+	});  
+});
+
+app.listen(80);
+
+console.log('Server running at http://testing.duostack.net:80/');
