45df31d93273733e42a854880147788bbc4a533a
Paul Thornton <ariochofmelnibone@gmail.com>
2011-03-15 22:28:55 +0000

    Automated Push

--- server.js
+++ server.js
@@ -85,9 +85,9 @@ mongoose.model("SpaceShipImage", SpaceShipImage);
 
 function get_count(callback) {
 	var model = mongoose.model("SpaceShipImage");
-	model.find({}).count(function(result) {
-			console.log("mongodb returned: "+result);
-			callback(result);
+	model.find({}, function(result) {
+			console.log("mongodb returned: "+result.length);
+			callback(result.length);
 	});
 }
 
